8af3d44d52f7b76fdacfb9a6d29c92a52524826c
shakilwebmaster <mdshakil102036@gmail.com>
2023-05-19 11:22:34 +0600

    navbar responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,24 @@
 //primary navigation bar active and hover color
-const btns = document.querySelectorAll(".p-btn");
-let primaryNavs = document.querySelector('#p-navs');
-const toggleBtn = document.querySelector('.toggle-btn');
+const btns = document.querySelectorAll(".main_list li");
+let mainListDiv = document.getElementById("mainListDiv"),
+    mediaButton = document.getElementById("mediaButton");
+console.log(btns)
 
 for(let i=0; i<btns.length; i++){
     btns[i].addEventListener("click", function(){
-        let currents= document.getElementsByClassName('active');
+        let currents = document.getElementsByClassName('active');
         currents[0].className = currents[0].className.replace("active", " ");
         this.className += "active";
+        console.log(currents[i])
     })
 }
 //navbar responsive
 
-toggleBtn.addEventListener('click', function(){
-    primaryNavs.classList.toggle('show')
-});
+mediaButton.onclick = function () {
+
+    "use strict";
+
+    mainListDiv.classList.toggle("show_list");
+    mediaButton.classList.toggle("active");
+
+};
